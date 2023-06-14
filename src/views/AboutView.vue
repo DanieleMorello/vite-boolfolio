07618a3ab7fff13e4ee49fc92343eef6afc2ff47
DanieleMorello <danielemorello@live.it>
2023-06-14 21:31:47 +0200

    Add an image and add content on about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,6 +10,20 @@ export default {
 
 <template>
   <AppBanner title="About"></AppBanner>
+  <section class="bio py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <img src="../assets/img/me.jpg" alt="Me">
+        </div>
+        <div class="col">
+          <h2>Daniele Morello</h2>
+          <p class="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore earum sapiente ipsam nisi aut
+            error! Soluta, culpa quis placeat, quidem non laudantium, nemo atque vel nostrum iure cum incidunt est!</p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style lang="scss" scoped></style>
