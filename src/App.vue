172f8da5f578ef2042db83df0ed2a0b49fbc9e61
DanieleMorello <danielemorello@live.it>
2023-06-14 20:43:32 +0200

    Create AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,13 @@
 <script>
-
-export default {}
+import AppHeader from './components/AppHeader.vue';
+export default {
+    components: { AppHeader }
+}
 
 </script>
 
 <template>
-    <header>
-        <nav class="navbar navbar-expand-sm navbar-light bg-light">
-            <div class="container">
-                <router-link class="navbar-brand" :to="{ 'name': 'home' }">Navbar</router-link>
-                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="collapsibleNavId">
-                    <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-                        <li class="nav-item">
-                            <router-link class="nav-link active" :to="{ 'name': 'home' }" aria-current="page">Home <span
-                                    class="visually-hidden">(current)</span></router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" :to="{ 'name': 'about' }">About</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" :to="{ 'name': 'contacts' }">Contacts</router-link>
-                        </li>
-                    </ul>
-
-                </div>
-            </div>
-        </nav>
-
-    </header>
+    <AppHeader></AppHeader>
     <router-view></router-view>
 </template>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<template>
+  <header class="shadow">
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+      <div class="container">
+        <router-link class="navbar-brand" :to="{ 'name': 'home' }">Navbar</router-link>
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+          <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link active" :to="{ 'name': 'home' }" aria-current="page">Home <span
+                  class="visually-hidden">(current)</span></router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ 'name': 'about' }">About</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ 'name': 'contacts' }">Contacts</router-link>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </nav>
+
+  </header>
+</template>
+
+
+<style lang="sass" scoped>
+
+</style>
